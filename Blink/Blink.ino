c0ec5000e9384e780a473dfd20a092d6ff45ae3d
Dylan bellinger <dylan.bellinger@gulllakecs.org>
2022-12-09 07:57:01 -0500

    Initial commit for ultrasonic security system

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,24 +1,61 @@
-/*
-  Blink
-Programer: Dylan Bellinger
-Date: 11.14.2022
-Program: Blink
+#define trigPin 2
+#define echoPin 3
+#define LEDlampRed 4
+#define LEDlampYellow 5
+#define LEDlampGreen 6 
+#define soundbuzzer 7
+int sound = 500;
 
-*/
 
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin 12 aand 11 an output.
-  pinMode(12, OUTPUT);  pinMode(11, OUTPUT);
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(LEDlampRed, OUTPUT);
+  pinMode(LEDlampYellow, OUTPUT);
+  pinMode(LEDlampGreen, OUTPUT);
+  pinMode(soundbuzzer, OUTPUT);
 }
-
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);                       // wait for a second
-  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(11, HIGH);    // turn the LED off by making the voltage LOW
-  delay(500);                       // wait for a second
-  digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
-   
+  long durationindigit, distanceincm;
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  durationindigit = pulseIn(echoPin, HIGH);
+  distanceincm = (durationindigit/5) / 29.1;
+ 
+  if (distanceincm < 50) {
+      digitalWrite(LEDlampGreen, HIGH);
+}
+  else {
+      digitalWrite(LEDlampGreen, LOW);
+  }
+  
+  if (distance < 20) {
+    digitalWrite(LEDlampYellow, HIGH);
+}
+  else {
+    digitalWrite(LEDlampYellow,LOW);
+  }
+  if (distance < 5) {
+    digitalWrite(LEDlampRed, HIGH);
+    sound = 1000;
+}
+  else {
+    digitalWrite(LEDlampRed,LOW);
+  }
+ 
+  if (distanceincm > 5 || distanceinsm <= 0){
+    Serial.println("Outside the permissible range of distances");
+    noTone(soundbuzzer);
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+    tone(buzzer, sound);
+  }
+  
+  delay(300);
 }
